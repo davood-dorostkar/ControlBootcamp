30819e42aa5435d00aa8dceb3e4f9196c5eee744
davood dorostkar <dado2hacker@gmail.com>
2022-07-14 21:56:49 +0430

    gramian: degrees of ctrb

---
+++ 02-gramian-ctrb.py
@@ -0,0 +1,25 @@
+from cgi import print_arguments
+from unicodedata import name
+# from numpy.linalg import svd
+from scipy.linalg import svd
+from control import ctrb
+
+A1 = [[1, 0], [0, 2]]
+B1 = [[0], [1]]
+
+A2 = [[1, 1], [0, 2]]
+B2 = [[0], [1]]
+
+
+def GetGramianCtrb(a, b):
+    c = ctrb(a, b)
+    u, s, v = svd(c)
+    print('\nGramian eig vectors: \n\n', u)
+    print('\nGramian eig values: \n\n', s)
+    print('======================\n')
+
+
+if __name__ == '__main__':
+    GetGramianCtrb(A1, B1)
+    GetGramianCtrb(A2, B2)
+    pass
